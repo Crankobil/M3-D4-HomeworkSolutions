bad4f2357acac831a5fc93568adb930d622d7745
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-08-12 16:00:54 +0100

    Amendments to cards

--- M3-D4_Script.js
+++ M3-D4_Script.js
@@ -14,15 +14,15 @@ const displayData = (data) => {
     let row = document.querySelector("#bookContainer")
     for ( let book of data){
         row.innerHTML +=  `
-        
         <div class="col-md-3">
         <div class="card-deck">
-        <div class="card">
-        <img src="${book.img}" class="card-img-top" alt="...">
+        <div class="card shadow-sm mb-4 h-100">
+        <img src="${book.img}" class="card-img-top" alt="Book Cover">
         <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <h5 class="card-title">${book.title}</h5>
+        <p class="card-text">$ ${book.price}</p>
         <a href="#" class="btn btn-primary">Add to cart</a>
+        
         </div>
         </div>
         </div>
